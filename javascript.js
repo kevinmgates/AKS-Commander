39c5efd53d8f63f46cdc97ffb71bdbfb6195f857
Kevin M. Gates <kevinmgates@users.noreply.github.com>
2020-06-11 06:42:02 -0500

    Baseline features with aad

---
+++ javascript.js
@@ -0,0 +1,4 @@
+function showOptions(source){
+    //alert(source.id);
+    document.getElementById(source.id + "-div").className = document.getElementById(source.id + "-div").className.replace("hide", "show");
+}
